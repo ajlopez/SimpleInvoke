3dfe94541fbc9484e266abd089d0d278e3dd884b
ajlopez <webmaster@ajlopez.com>
2013-01-21 07:43:08 -0300

    Invoke object function

--- test/invoke.js
+++ test/invoke.js
@@ -4,6 +4,8 @@ var si = require('..'),
 
 var ninvokes = 0;
 
+// Invoke simple function with callback
+
 si.invoke(
     function (a, b, cb) { assert.equal(a, 1); assert.equal(b, 2); ninvokes++; cb(null, a+b); },
     [1, 2]
@@ -11,13 +13,35 @@ si.invoke(
 
 assert.equal(ninvokes, 1);
 
-ninvokes =0;
+// Invoke two functions with callback
+
+ninvokes = 0;
 
 si.invoke(
-    function (a, b, cb) { assert.equal(a, 1); assert.equal(b, 2); ninvokes++; cb(null, a+b); },
+    function (a, b, cb) { assert.equal(a, 1); assert.equal(b, 2); assert.equal(ninvokes, 0); ninvokes++; cb(null, a+b); },
     [1, 2],
-    function (a, b, cb) { assert.equal(a, 1); assert.equal(b, 2); ninvokes++; cb(null, a+b); },
+    function (a, b, cb) { assert.equal(a, 1); assert.equal(b, 2); assert.equal(ninvokes, 1); ninvokes++; cb(null, a+b); },
     [1, 2]
 );
 
 assert.equal(ninvokes, 2);
+
+// Invoke object function
+
+ninvokes = 0;
+
+var obj = {
+    x: 1,
+    add: function (a, b, cb) {
+        assert.ok(this);
+        assert.equal(this, obj);
+        assert.equal(this.x, 1);
+        ninvokes++;
+    }
+};
+
+si.invoke(obj, obj.add, [1, 2]);
+
+assert.equal(ninvokes, 1);
+
+
